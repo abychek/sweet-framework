b6ba7a486f88890f800af8f420db68e391cd2099
kravchenko.marina <ab_bie@mail.ru>
2017-04-17 21:41:23 +0300

    add twig

---
+++ src/Controllers/DefaultController.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: marina
+ * Date: 16.04.17
+ * Time: 10:35
+ */
+
+namespace SweetFramework\Controllers;
+
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class DefaultController
+{
+    public function helloAction(Request $request, $name = 'world')
+    {
+        return new Response("Hello " . $name);
+    }
+
+    public function byeAction(Request $request, $name = 'world')
+    {
+        return new Response("Bye " . $name);
+    }
+}
