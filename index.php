9460ed9a826c9f73f3ce8d7136560acd304f2ec0
Alexey Bychek <alexey.bychek94@gmail.com>
2017-04-22 10:22:09 +0300

    Added our classes to composer autoload.

--- index.php
+++ index.php
@@ -1,7 +1,6 @@
 <?php
 
 require_once __DIR__ . '/vendor/autoload.php';
-require_once __DIR__ . '/src/Controllers/DefaultController.php';
 
 use Pimple\Container;
 use Symfony\Component\HttpFoundation\Request;
